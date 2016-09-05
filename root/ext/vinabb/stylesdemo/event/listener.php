4b3d8bef80bc94d3c476abed7f2842f2e223d7f7
Khoa Nguyen <nedkavn@gmail.com>
2016-09-05 22:24:42 +0700

    Why do need events when we have a whole code?

--- root/ext/vinabb/stylesdemo/event/listener.php
+++ root/ext/vinabb/stylesdemo/event/listener.php
@@ -12,9 +12,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
-	/** @var \phpbb\db\driver\driver_interface */
-    protected $db;
-
 	/** @var \phpbb\cache\driver\driver_interface */
 	protected $cache;
 
@@ -29,9 +26,6 @@ class listener implements EventSubscriberInterface
 
 	/** @var \phpbb\user */
     protected $user;
-
-	/** @var \phpbb\request\request */
-    protected $request;
 
 	/** @var \phpbb\extension\manager */
 	protected $ext_manager;
@@ -51,38 +45,32 @@ class listener implements EventSubscriberInterface
 	/**
 	* Constructor
 	*
-	* @param \phpbb\db\driver\driver_interface $db
 	* @param \phpbb\cache\driver\driver_interface $cache
 	* @param \phpbb\config\config $config
 	* @param \phpbb\controller\helper $helper
 	* @param \phpbb\template\template $template
 	* @param \phpbb\user $user
-	* @param \phpbb\request\request $request
 	* @param \phpbb\extension\manager $ext_manager
 	* @param \phpbb\path_helper $path_helper
 	* @param string $phpbb_root_path
 	* @param string $php_ext
 	*/
-	public function __construct(\phpbb\db\driver\driver_interface $db,
-								\phpbb\cache\driver\driver_interface $cache,
+	public function __construct(\phpbb\cache\driver\driver_interface $cache,
 								\phpbb\config\config $config,
 								\phpbb\controller\helper $helper,
 								\phpbb\template\template $template,
 								\phpbb\user $user,
-								\phpbb\request\request $request,
 								\phpbb\extension\manager $ext_manager,
 								\phpbb\path_helper $path_helper,
 								$phpbb_root_path,
 								$phpbb_admin_path,
 								$php_ext)
 	{
-		$this->db = $db;
 		$this->cache = $cache;
 		$this->config = $config;
 		$this->helper = $helper;
 		$this->template = $template;
 		$this->user = $user;
-		$this->request = $request;
 		$this->ext_manager = $ext_manager;
 		$this->path_helper = $path_helper;
 		$this->phpbb_root_path = $phpbb_root_path;
@@ -96,77 +84,10 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.user_setup'				=> 'update_lang',
-			'core.user_setup_after'			=> 'redirect_to_demo',
 			'core.page_header'				=> 'add_page_header_link',
 			'core.adm_page_header_after'	=> 'update_tpl_vars',
 			'core.add_log'					=> 'purge_lang_cache',
 		);
-	}
-
-	/**
-	* Switch languages if the parameter &l=1
-	*
-	* @param $event
-	*/
-	public function update_lang($event)
-	{
-		$switch_lang = $this->request->variable('l', false);
-		$last_page = str_replace('&amp;', '&', $this->request->variable('z', ''));
-
-		if ($this->user->data['user_id'] == ANONYMOUS)
-		{
-			$demo_lang = $this->request->variable($this->config['cookie_name'] . '_lang', '', true, \phpbb\request\request_interface::COOKIE);
-			$demo_lang = empty($demo_lang) ? $this->config['default_lang'] : $demo_lang;
-		}
-		else
-		{
-			$demo_lang = $this->user->data['user_lang'];
-		}
-
-		// Need to switch to another language?
-		if ($switch_lang && $this->config['vinabb_stylesdemo_lang_enable'] && !empty($this->config['vinabb_stylesdemo_lang_switch']) && $this->config['vinabb_stylesdemo_lang_switch'] != $this->config['default_lang'])
-		{
-			$new_lang = ($demo_lang == $this->config['default_lang']) ? $this->config['vinabb_stylesdemo_lang_switch'] : $this->config['default_lang'];
-
-			if ($this->user->data['user_id'] == ANONYMOUS)
-			{
-				$this->user->set_cookie('lang', $new_lang, 0, false);
-			}
-			else
-			{
-				$sql = 'UPDATE ' . USERS_TABLE . "
-					SET user_lang = '$new_lang'
-					WHERE user_id = " . $this->user->data['user_id'];
-				$this->db->sql_query($sql);
-			}
-
-			$response = new \Symfony\Component\HttpFoundation\RedirectResponse(
-				append_sid($this->phpbb_root_path . $last_page),
-				301
-			);
-			$response->send();
-		}
-	}
-
-	/**
-	* Redirect to the demo page if any visitors go to our demo board directly
-	*
-	* @param $event
-	*/
-	public function redirect_to_demo($event)
-	{
-		$style = $this->request->variable('style', 0);
-		$acp_style = $this->request->variable('s', '');
-
-		if (!$style && !$acp_style && !in_array($this->user->page['page_name'], array("app.{$this->php_ext}/demo/", "app.{$this->php_ext}/demo/acp", "app.{$this->php_ext}/demo/acp/")) && $this->user->data['user_type'] != USER_FOUNDER && !defined('IN_LOGIN'))
-		{
-			$response = new \Symfony\Component\HttpFoundation\RedirectResponse(
-				$this->helper->route('vinabb_stylesdemo_route', array('mode' => '')),
-				301
-			);
-			$response->send();
-		}
 	}
 
 	/**
