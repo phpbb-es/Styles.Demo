dec004b37b7a7551b2c8864c6a9121ad754843d2
Khoa Nguyen <nedkavn@gmail.com>
2016-12-19 18:58:16 +0700

    Add new constants

--- includes/constants.php
+++ includes/constants.php
@@ -13,6 +13,9 @@ class constants
 	// Admin roles
 	const ROLE_ADMIN_DEMO = 'ROLE_ADMIN_DEMO';
 
+	// Username of the anonymous account
+	const ANONYMOUS_USERNAME = 'Anonymous';
+
 	// Screenshot types
 	const SCREENSHOT_EXT = '.png';
 	const SCREENSHOT_TYPE_LOCAL = 0;
@@ -30,6 +33,7 @@ class constants
 	// Styles
 	const DEFAULT_STYLE = 'prosilver';// Do not change it! This is not the default style of your board.
 	const STYLES_PER_PAGE = 20;
+	const MAX_CONFIG_NAME = 255;
 
 	// Currency symbol
 	const CURRENCY_SYMBOL = '$%d';
